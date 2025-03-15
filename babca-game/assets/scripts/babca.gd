751f445c53958b7cf718b42fa536059a861b7e8e
Ondroid91 <ondramonkey@seznam.cz>
2025-03-15 16:12:41 +0100

    player animace

--- babca-game/assets/scripts/babca.gd
+++ babca-game/assets/scripts/babca.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 # PLAYER COMPONENTS----------------------------------------------- #
 @export var body_collision : CollisionShape2D
-@export var sprite : Sprite2D
+@export var player_sprite : AnimatedSprite2D
 @export var ground_ray : RayCast2D
 # ---------------------------------------------------------------- #
 
@@ -19,6 +19,7 @@ func _physics_process(delta: float) -> void:
 	refresh()
 
 func player_movement(delta: float) -> void:
+	#movement
 	velocity.y += self.get_gravity().y * delta  
 	var input_direction = Input.get_axis("left", "right")
 	if input_direction != 0:
@@ -26,10 +27,39 @@ func player_movement(delta: float) -> void:
 	else:
 		velocity.x = lerp(velocity.x, 0.0, friction * delta)
 	if Input.is_action_pressed("up") and ground_ray.is_colliding():
-		print("jump")
 		velocity.y -= jump_speed
 	move_and_slide()
+	# animations
+	if ground_ray.is_colliding():
+		match input_direction:
+			-1.0:
+				play_animation("walk", false)
+			1.0:
+				play_animation("walk", true)
+			0.0:
+				play_animation("idle")
+	else:
+		match input_direction:
+			-1.0:
+				if velocity.y < 0:
+					play_animation("jump", false)
+				elif velocity.y > 0:
+					play_animation("fall", false)
+			1.0:
+				if velocity.y < 0:
+					play_animation("jump", true)
+				elif velocity.y > 0:
+					play_animation("fall", true)
 
+	
 func refresh() -> void:
 	if Input.is_action_just_pressed("refresh"):
 		get_tree().reload_current_scene()
+
+func play_animation(anim : String, flip : bool = player_sprite.flip_h) -> void:
+	player_sprite.flip_h = flip
+	player_sprite.play(anim)
+	
+	
+	
+	
