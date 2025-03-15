a410584751ddcb6d09937fa5e4661bc7c3b93e40
Ondroid91 <ondramonkey@seznam.cz>
2025-03-15 10:59:59 +0100

    loading windows

--- babca-game/assets/scripts/okno.gd
+++ babca-game/assets/scripts/okno.gd
@@ -9,6 +9,7 @@ extends Node2D
 @export var animation_player : AnimationPlayer
 @export var sound_player : AudioStreamPlayer2D
 @export var static_coll : StaticBody2D
+@export var area_coll : Area2D
 @export var max_coll : CollisionPolygon2D
 @export var min_coll : CollisionShape2D
 @export var cancel_button : Button
@@ -17,8 +18,8 @@ extends Node2D
 
 # ------------ WINDOW TYPE -----------------------
 @export var window_type : String
-@export var cancel_enabled : bool
-@export var min_enabled : bool
+@export var cancel_enabled : bool = true
+@export var min_enabled : bool = true
 @export var move_ver_enebled : bool = false
 @export var move_hor_enebled : bool = false
 
@@ -40,6 +41,10 @@ func _ready():
 	if move_button:
 		move_button.button_down.connect(_on_move_button_pressed)
 		move_button.button_up.connect(_on_move_button_released)
+	if area_coll:
+		area_coll.body_entered.connect(_on_body_entered)
+
+	update_buttons()
 
 func _physics_process(delta: float) -> void:
 	if window_moving:
@@ -49,21 +54,58 @@ func _physics_process(delta: float) -> void:
 		elif move_hor_enebled:
 			self.position.x = mouse_pos.x - offset_x
 
-func _on_cancel_button_pressed():
-	print("ljdslkfajlkds")
+
+
+func update_buttons() -> void:
+	if not cancel_enabled:
+		button_animtion(ani_sprite_cancel_button, "default", 3)
+	if is_minimalized:
+		var anim = "maximaze"
+		if min_enabled:
+			button_animtion(ani_sprite_min_button, anim, 0)
+		else:
+			button_animtion(ani_sprite_min_button, anim, 3)
+	else:
+		var anim = "minimaze"
+		if min_enabled:
+			button_animtion(ani_sprite_min_button, anim, 0)
+		else:
+			button_animtion(ani_sprite_min_button, anim, 3)
+
+
+func button_animtion(button : AnimatedSprite2D,anim : String, fram : int) -> void:
+	button.animation = anim
+	button.frame = fram
+
+
+func loading_start() -> void:
+	ani_sprite_window.play()
+	await get_tree().create_timer(2.0).timeout
 	queue_free()
 
+
+func _on_body_entered(body):
+	if body.name == "babca":
+		loading_start()
+
+
+func _on_cancel_button_pressed():
+	if cancel_enabled:
+		queue_free()
+
 func _on_min_button_pressed():
-	if is_minimalized:
-		is_minimalized = false
-		min_coll.disabled = false
-		max_coll.disabled = true
-		ani_sprite_window.frame = 0
-	elif not is_minimalized:
-		is_minimalized = true
-		min_coll.disabled = true
-		max_coll.disabled = false
-		ani_sprite_window.frame = 1
+	if min_enabled:
+		if is_minimalized:
+			is_minimalized = false
+			min_coll.disabled = false
+			max_coll.disabled = true
+			ani_sprite_window.frame = 0
+		elif not is_minimalized:
+			is_minimalized = true
+			min_coll.disabled = true
+			max_coll.disabled = false
+			ani_sprite_window.frame = 1
+		update_buttons()
 
 func _on_move_button_pressed():
 	window_moving = true
