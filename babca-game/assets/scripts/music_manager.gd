c0638560af729b8da4dbada21a7548d5c95b26bd
Ondroid91 <ondramonkey@seznam.cz>
2025-03-16 01:05:02 +0100

    music and background

--- babca-game/assets/scripts/music_manager.gd
+++ babca-game/assets/scripts/music_manager.gd
@@ -1,12 +1,8 @@
-extends Node
+extends AudioStreamPlayer
 
-@export var music_player : AudioStreamPlayer2D
 @export var menu_oundtrack : AudioStream
 @export var main_soundtrack : AudioStream
 
-
-func _ready():
-	music_player.play()
 
 func start_music(where_am_i : String) -> void:  # game, menu
 	var music : AudioStream
@@ -14,5 +10,5 @@ func start_music(where_am_i : String) -> void:  # game, menu
 		music = main_soundtrack
 	elif where_am_i == "menu":
 		music = menu_oundtrack
-	music_player.stream = music
-	music_player.play()
+	stream = music
+	play()
