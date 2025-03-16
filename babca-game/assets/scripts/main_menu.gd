3de3ab326ceb3e338f4f761c6f24689c3d48c268
Ondroid91 <ondramonkey@seznam.cz>
2025-03-16 02:50:19 +0100

    main menu

---
+++ babca-game/assets/scripts/main_menu.gd
@@ -0,0 +1,38 @@
+extends CanvasLayer
+
+@export var ani : AnimatedSprite2D
+var scene_to_load = "uid://7suyqljufqur"
+
+func _ready() -> void:
+	ms.start_music("menu")
+
+func exist() -> void:
+	ani.frame = 0
+
+func click() -> void:
+	ani.frame = 1
+	await get_tree().create_timer(0.5)
+	get_tree().change_scene_to_file(scene_to_load)
+	
+func touch() -> void:
+	ani.frame = 2
+
+
+
+func _on_button_pressed() -> void:
+	click()
+
+func _on_button_mouse_entered() -> void:
+	touch()
+
+func _on_button_mouse_exited() -> void:
+	exist()
+
+func _on_button_2_pressed() -> void:
+	click()
+
+func _on_button_2_mouse_entered() -> void:
+	touch()
+
+func _on_button_2_mouse_exited() -> void:
+	exist()
---
+++ babca-game/assets/scripts/pdf.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var collect_sound : AudioStreamPlayer2D
+@export var image : AnimatedSprite2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "babca":
+		sv.have_recept = true
+		collect_sound.play()
+		image.visible = false
+		await get_tree().create_timer(2.0).timeout
+		queue_free()
